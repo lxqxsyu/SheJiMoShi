2c97b5cafd3ea6882b34e2c33d424fb8bc342b4e
lxqxsyu <lxq_xsyu@163.com>
2014-09-30 15:31:49 +0800

    生产消费

---
+++ src/生产消费模型/Test.java
@@ -0,0 +1,20 @@
+package 生产消费模型;
+
+public class Test {
+	
+	/**
+	 * 仓库
+	 * @author Administrator
+	 *
+	 */
+	class Godown{
+		public static final int max_size = 100; //最大容量
+		public int currnum;  //当前存储量
+		
+		public Godown(int currnum) {
+			this.currnum = currnum;
+		}
+		
+		public produce
+	}
+}
